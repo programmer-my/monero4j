7612de908aaa60ca04e1037e36efca64fcaf0fcf
Aiman Daniel <aiman2301@gmail.com>
2024-04-26 08:53:19 +0800

    chore: initial commit

---
+++ rpc-client/build.gradle.kts
@@ -0,0 +1,21 @@
+group = "my.programmer"
+//name = "rpc-client"
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
