9bd8bd7155c611c9bdbfbdd66c173bdd33ae8f49
Aiman Daniel <aiman2301@gmail.com>
2024-04-26 13:29:48 +0800

    chore: resolve todo

--- rpc-client/src/main/java/my/programmer/monero4j/rpc_client/response/GetBlockHeaderByHashResponse.java
+++ rpc-client/src/main/java/my/programmer/monero4j/rpc_client/response/GetBlockHeaderByHashResponse.java
@@ -1,6 +1,5 @@
 package my.programmer.monero4j.rpc_client.response;
 
-// TODO: extract BlockHeader into a new class
-// OR make all get_*_block_header_* response use the same response
+// TODO: make all get_*_block_header_* response use the same response?
 public class GetBlockHeaderByHashResponse extends GetLastBlockHeaderResponse {
 }
