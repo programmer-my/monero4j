15feea3c288e84639d585540ab837d10cf7ce9f0
Aiman Daniel <aiman2301@gmail.com>
2024-04-29 00:48:51 +0800

    deps: io.github.rburgst:okhttp-digest:3.1.0

--- rpc-client/build.gradle.kts
+++ rpc-client/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    implementation("io.github.rburgst:okhttp-digest:3.1.0")
 }
 
 tasks.named<Test>("test") {
